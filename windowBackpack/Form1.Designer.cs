cca25b6e2afbd600493bcd9eee516f88f27944dd
Student 254014 <254014@student.pwr.edu.pl>
2022-03-15 20:32:20 +0100

    Forms

---
+++ windowBackpack/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace windowBackpack
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.quantityLabel = new System.Windows.Forms.Label();
+            this.quantity = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.seedLabel = new System.Windows.Forms.Label();
+            this.seed = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(130, 35);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.quantity_Click);
+            // 
+            // quantityLabel
+            // 
+            this.quantityLabel.AutoSize = true;
+            this.quantityLabel.Location = new System.Drawing.Point(39, 16);
+            this.quantityLabel.Name = "quantityLabel";
+            this.quantityLabel.Size = new System.Drawing.Size(55, 16);
+            this.quantityLabel.TabIndex = 1;
+            this.quantityLabel.Text = "Quantity";
+            this.quantityLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.quantityLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // quantity
+            // 
+            this.quantity.Location = new System.Drawing.Point(16, 35);
+            this.quantity.Name = "quantity";
+            this.quantity.Size = new System.Drawing.Size(100, 22);
+            this.quantity.TabIndex = 2;
+            this.quantity.TextChanged += new System.EventHandler(this.quantity_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(130, 82);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.seedConfirm_Click);
+            // 
+            // seedLabel
+            // 
+            this.seedLabel.AutoSize = true;
+            this.seedLabel.Location = new System.Drawing.Point(41, 63);
+            this.seedLabel.Name = "seedLabel";
+            this.seedLabel.Size = new System.Drawing.Size(40, 16);
+            this.seedLabel.TabIndex = 4;
+            this.seedLabel.Text = "Seed";
+            this.seedLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.seedLabel.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // seed
+            // 
+            this.seed.Location = new System.Drawing.Point(16, 82);
+            this.seed.Name = "seed";
+            this.seed.Size = new System.Drawing.Size(100, 22);
+            this.seed.TabIndex = 5;
+            this.seed.TextChanged += new System.EventHandler(this.seed_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.textBox3.Location = new System.Drawing.Point(225, 36);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox3.Size = new System.Drawing.Size(563, 366);
+            this.textBox3.TabIndex = 6;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(16, 129);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(189, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.seed);
+            this.Controls.Add(this.seedLabel);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.quantity);
+            this.Controls.Add(this.quantityLabel);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label quantityLabel;
+        private System.Windows.Forms.TextBox quantity;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label seedLabel;
+        private System.Windows.Forms.TextBox seed;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ windowBackpack/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Lab1dotNet;
+namespace windowBackpack
+{
+    public partial class Form1 : Form
+    {
+        bool qFilled = false;
+        bool sFilled = false;
+
+        int q = 0;
+        int s = 1;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void seed_TextChanged(object sender, EventArgs e)
+        {
+            if (seed.Text == "")
+                sFilled = false;
+            else
+                sFilled = true;
+        }
+
+        private void quantity_TextChanged(object sender, EventArgs e)
+        {
+            if(quantity.Text == "")
+                qFilled = false;
+            else
+                qFilled = true;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //textBox3.Text = "q= " + q + Environment.NewLine
+            //                + "s= " + s + Environment.NewLine;
+
+            
+            int n = q;
+            
+            int seed = s;
+            Generator g = new Generator(n, seed);
+            Backpack b = new Backpack();
+
+
+
+
+            b.Algorithm(g);
+
+            textBox3.Text = g + Environment.NewLine
+                            + b + Environment.NewLine;
+            
+            
+            
+
+        }
+
+        private void seedConfirm_Click(object sender, EventArgs e)
+        {
+            if (sFilled)
+                s = Convert.ToInt32(seed.Text);
+        }
+
+        private void quantity_Click(object sender, EventArgs e)
+        {
+            if(qFilled)
+                q = Convert.ToInt32(quantity.Text);
+        }
+    }
+}
