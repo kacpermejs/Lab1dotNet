04f61bf49bd6cd630454a495e17ed020fd595020
Piotr Markiewicz <62255466+pio-mark@users.noreply.github.com>
2022-03-16 11:43:23 +0100

    Testy 3/3

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -8,13 +8,42 @@ namespace UnitTestProject1
     public class UnitTest1
     {
         [TestMethod]
-        public void Item_tet()
+        public void Item_test_count()
         {
             Generator g = new Generator(10, 10);
 
             var result = g.items.Count;
 
             Assert.AreEqual(10, result);
+        }
+
+        [TestMethod]
+        public void Backpack_test_count()
+        {
+            Backpack b = new Backpack();
+            Generator g = new Generator(10, 10);
+
+            for(int i = 0; i < 10; i++)
+            {
+                b.AddItem(g.items[i]);
+            }
+            var result = b.contents.Count;
+
+            Assert.AreEqual(result, 10);
+        }
+
+        [TestMethod]
+        public void Test3()
+        {
+            int itemValue = 10;
+            int itemWeight = 10;
+            Item item = new Item(itemValue, itemWeight);
+            var result1 = item.Value;
+            var result2 = item.Weight;
+            
+            Assert.AreEqual(itemValue, result1);
+            Assert.AreEqual(itemWeight, result2);
+            Assert.IsTrue(result1 == result2);
         }
 
 
