682a8267fa7309150bba4e217239e76f204284ff
Piotr Markiewicz <62255466+pio-mark@users.noreply.github.com>
2022-03-16 11:15:24 +0100

    Test 1.

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -8,8 +8,18 @@ namespace UnitTestProject1
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Item_tet()
         {
+            Generator g = new Generator(10, 10);
+
+            var result = g.items.Count;
+
+            Assert.AreEqual(10, result);
         }
+
+
+
+
+
     }
 }
